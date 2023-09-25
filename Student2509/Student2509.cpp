e97e22c857d8c872f5d8a863ec0b5d634dd091d5
Mirovinkin <vaheabroyan3@gmail.com>
2023-09-25 20:40:48 +0500

    Добавьте файлы проекта.

---
+++ Student2509/Student2509.cpp
@@ -0,0 +1,41 @@
+﻿// Student2509.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <windows.h>
+#include <locale.h>
+#include <string.h>
+#include <typeinfo>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+//#include <cstdlib>
+#include <ctype.h>
+
+using namespace std;
+
+class Student {
+private:
+    string _name;
+    unsigned _age;
+    double _avMark;
+
+public:
+    Student(const string& Name, const unsigned& Age, const double& AvMark) :
+        _name(Name), _age(Age), _avMark(AvMark) {};
+    void Print() {
+        if (_name == "" || _age == NAN || _avMark == NAN) {
+            cout << "False info, try again";
+        }
+        else {
+            cout << "Name: " << _name << "\nAge: " << _age << "\nAverage mark: " << _avMark;
+        }
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
